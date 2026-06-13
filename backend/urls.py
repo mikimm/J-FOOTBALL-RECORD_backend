ec4c44234d050e856b6afb332e5520481172fbc6
mikihiromakino <16id014mmiki@gmail.com>
2026-06-13 23:29:18 +0900

    正規表現修正

--- backend/urls.py
+++ backend/urls.py
@@ -17,11 +17,18 @@ Including another URLconf
 from django.urls import include, path,re_path
 from django.shortcuts import render
 from django.contrib.staticfiles.views import serve
+from jfootball_record.views.sign_up_view import SignupView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
+@login_required
 def index_view(request,*args, **kwargs):
     return render(request, 'index.html')
 
 urlpatterns = [
+    path('sign_up/', SignupView.as_view()),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('api/v1/', include('jfootball_record.urls')),
-    re_path(r'^my-app/(?!.*images/).*$', index_view, name='index'), 
-    re_path(r'^my-app/(?P<path>.*?\.[^/]+)$',serve),
+    re_path(r'^my_app/(?!.*(images/|static/)).*$', index_view, name='index'), 
+    re_path(r'^my_app/(?P<path>.*?\.[^/]+)$',serve),
 ]
