9b6d69c45424959f98adc8663d5a657d373972ae
mikihiromakino <16id014mmiki@gmail.com>
2026-09-04 10:19:23 +0900

    コメント一覧API実装

--- jfootball_record/views/comments_views.py
+++ jfootball_record/views/comments_views.py
@@ -1,12 +1,14 @@
 from rest_framework import generics
+from jfootball_record.model_definition.comments_models import Comments
 from jfootball_record.serializer.comments_serializer import CommentsSerializer
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 
-class CommentsView(generics.CreateAPIView):
+class CommentsView(generics.ListCreateAPIView):
     serializer_class = CommentsSerializer
+    queryset = None
     # authentication_classes = (SessionAuthentication,)
     # permission_classes = (IsAuthenticated, )
     def create(self,request, *args, **kwargs):
@@ -18,3 +20,13 @@ class CommentsView(generics.CreateAPIView):
     def perform_create(self, serializer,user_id):
         record_id = self.kwargs['record_id']
         serializer.save(record_id=record_id,comment_by_id=user_id)
+    # コメント一覧をrecord_idで絞り込み
+    def get(self, request, *args, **kwargs):
+        record_id = self.kwargs['record_id']
+        self.queryset = Comments.objects.filter(record_id=record_id)
+        return self.list(request, *args, **kwargs)
+    # コメントの総数を返すようにオーバーライド
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({"count":self.queryset.count(),"comments":serializer.data})
