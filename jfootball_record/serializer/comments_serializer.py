333339f0ce914090f37c79c8e8341bceb8e6d2c5
mikihiromakino <16id014mmiki@gmail.com>
2026-09-06 06:43:52 +0900

    コメント機能改修

--- jfootball_record/serializer/comments_serializer.py
+++ jfootball_record/serializer/comments_serializer.py
@@ -2,9 +2,14 @@ from rest_framework import serializers
 
 from django.db import models
 from jfootball_record.model_definition.comments_models import Comments
+from jfootball_record.model_definition.users_models import Users
 
 # Comments用シリアライザー
 class CommentsSerializer(serializers.ModelSerializer):
+    comment_by = serializers.CharField(
+        source="comment_by.username",
+        read_only=True
+    )
     class Meta:
         model=Comments
-        exclude = ['record','comment_by']
+        exclude = ['record']
